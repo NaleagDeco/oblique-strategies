d69183b7b25d81f2e985aa1266665669a89f3393
Gaelan D'costa <gdcosta@gmail.com>
2014-12-10 00:18:20 -0500

    Bugfixes.

--- oblique-strategies.lisp
+++ oblique-strategies.lisp
@@ -10,26 +10,31 @@
 
 (defun draw-card (&optional edition)
   "Draw random card, from specific edition if specified"
-  (let ((uri (concatenate 'string *api-root-url*
+  (let* ((edition-str (write-to-string edition))
+        (uri (concatenate 'string *api-root-url*
                           (if (not (null edition))
-                              (concatenate 'string "/edition/" edition))
+                              (concatenate 'string "/edition/" edition-str))
                           "/draw")))
     (yason:parse (drakma:http-request uri :want-stream T))))
 
 (defun get-all-cards (edition)
   "List of all available cards for a given edition"
-  (let ((uri (concatenate 'string *api-root-url* "/edition/" edition "/all")))
+  (let* ((edition-str (write-to-string edition))
+        (uri (concatenate 'string *api-root-url* "/edition/" edition-str "/all")))
     (yason:parse (drakma:http-request uri :want-stream T))))
 
 (defun get-card-by-number (edition card-number)
   "Get nth card from the specific edition"
-  (let ((url (concatenate 'string *api-root-url*
-                          "/edition/" edition "/cardnumber/" card-number)))
+  (let* ((edition-str (write-to-string edition))
+        (card-number-str (write-to-string card-number))
+        (url (concatenate 'string *api-root-url*
+                          "/edition/" edition-str "/cardnumber/" card-number-str)))
     (yason:parse (drakma:http-request url :want-stream T))))
 
 (defun get-card-by-id (id)
   "Get card by API Server ID"
-  (let ((url (concatenate 'string *api-root-url* "/id/" id)))
+  (let* ((id-str (write-to-string id))
+        (url (concatenate 'string *api-root-url* "/id/" id-str)))
     (yason:parse (drakma:http-request url :want-stream T))))
 
 (defun debug-card (card)
@@ -39,13 +44,13 @@
     (maphash #'print-hash-entry card)))
 
 (defun strategy (card)
-  (gethash 'strategy card))
+  (gethash "strategy" card))
 
 (defun edition (card)
-  (gethash 'edition card))
+  (gethash "edition" card))
 
 (defun id (card)
-  (gethash 'id card))
+  (gethash "id" card))
 
 (defun card-number (card)
-  (gethash 'cardnumber card))
+  (gethash "cardnumber" card))
